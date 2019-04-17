b6328d564e806b7261030cc536f983bf258b6a36
JoseaneMansur <joseane.mansur@estudante.ufla.br>
2019-04-17 09:12:40 -0300

    Create classeB.java
    ClasseB-ES

---
+++ classeB.java
@@ -0,0 +1,33 @@
+package projeto_joseane;
+
+/**
+ * @author Josi
+ */
+public class classeB {
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+    
+    public void mb1(){
+        System.out.println("mb1");
+    }
+    
+    public void mb2(){
+        System.out.println("mb2");
+    }
+}
