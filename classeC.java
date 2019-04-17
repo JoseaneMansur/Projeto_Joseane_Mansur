b1fb918c56285447d8cc6dbd1474aecf575c4a7b
JoseaneMansur <joseane.mansur@estudante.ufla.br>
2019-04-17 09:25:48 -0300

    Create classeC.java
    criacao de classe c

---
+++ classeC.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Josi
+ */
+public class classeC {
+    private String C1;
+    private int C2;
+    
+    public void mc1(){
+        System.out.println("mc1");
+    }
+    
+    public void mc2(){
+        System.out.println("mc2");
+    }
+}
