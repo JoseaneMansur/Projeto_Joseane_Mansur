9dc3c1181188f4e389dfd6aa9bb76313fb91e1c2
JoseaneMansur <joseane.mansur@estudante.ufla.br>
2019-04-17 09:12:29 -0300

    Create classeA.java
    ClasseA-ES

---
+++ classeA.java
@@ -0,0 +1,33 @@
+package projeto_joseane;
+
+/**
+ * @author Josi
+ */
+public class classeA {
+    private int A1;
+    private float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+    
+    public void ma1(){
+        System.out.println("ma1");
+    }
+    
+    public void ma2(){
+        System.out.println("mb2");
+    }
+}
